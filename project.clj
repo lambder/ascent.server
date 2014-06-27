ddb9fb8b84d6fc82407e698a9d7e20a5a7205c18
Ilya Shinkarenko <shinych@gmail.com>
2014-06-27 20:36:11 +0200

    streamline clojurescript version: 0.0-2234

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   
   :dependencies [
   	[org.clojure/clojure "1.5.1"]
-    [org.clojure/clojurescript "0.0-2156"]
+    [org.clojure/clojurescript "0.0-2234"]
     [http-kit "2.1.18"]
     [jarohen/chord "0.3.1"]
     [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
